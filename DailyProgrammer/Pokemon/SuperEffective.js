5e94a3810e7ff7269bada70c295c96713a34547f
Capen Rhew <capenr@gmail.com>
2017-02-07 21:35:18 -0500

    Finished Implementation
    Program now works as expected.  Takes a single type against another
    single type and outputs effectiveness.

--- DailyProgrammer/Pokemon/SuperEffective.js
+++ DailyProgrammer/Pokemon/SuperEffective.js
@@ -10,8 +10,26 @@ The program should output the damage multiplier these types lead to.
 "use strict";
 
 const http = require('http');
-//const fs = require('fs');
-console.log("Starting...");
+const readline = require('readline');
+const regEx = /^[a-z]+\s->\s[a-z]+$/;
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+
+console.log("Welcome to Super Effective!");
+console.log("Please enter a type matchup in format \"<attacking type> -> <defending type>\"");
+rl.prompt();
+rl.on('line', function(input) {
+	
+	let found = input.match(regEx);
+	if (found !== null) {
+		let types = found[0].split(' -> ');
+		getPokemonData(types[0], types[1]);
+	}
+	rl.prompt();
+});
 
 function getPokemonData(typeUsed, typeUsedAgainst) {
 	const url = 'http://pokeapi.co/api/v2/type/' + typeUsed + '/'; 
@@ -37,34 +55,55 @@ function getPokemonData(typeUsed, typeUsedAgainst) {
 		
 		let body = '';
 		response.on('data', function(d) {
-			console.log("fetching response");
 			body += d;
 		});
 		
 		response.on('end', function() {
 			let dmgArr = [];
-			console.log("printing response");
-			// Output to file for testing. 
-			// TODO: Delete later.
-			/**fs.writeFile('output.json', body, function(err) {
-				if (err) {
-					console.error(err);
-				}
-			});**/
 			const parsed = JSON.parse(body);
+			/** 
+			* Create a 2d array where each of the three inner arrays
+			* corresponds to a damage multiplier.
+			* If a type is not in one of the three inner arrays, it can
+			* be assumed that the given type does normal damage to the type
+			* it used against.
+			**/
 			dmgArr.push(parsed.damage_relations.no_damage_to);
 			dmgArr.push(parsed.damage_relations.half_damage_to);
 			dmgArr.push(parsed.damage_relations.double_damage_to);
-			calculateDmg(dmgArr, typeUsedAgainst);
+			console.log(calculateDmg(dmgArr, typeUsedAgainst) + "x");
 		});
 	});
 }
 
 function calculateDmg(damageArray, typeUsedAgainst) {
-	console.log(damageArray);
-	console.log("Used against: " + typeUsedAgainst);
-}
+	// search the 2d array for the type the move is being used against
+	// when found return the inner array in which the type was found
+	function findTypeUsedAgainst() {
+		for (let i = 0; i < damageArray.length; i++) {
+			for (let j = 0; j < damageArray[i].length; j++) {
+				if (damageArray[i][j].name === typeUsedAgainst) {
+					return i;
+				}
+			}
+		}
+	}
 
-getPokemonData('fire', 'grass');
-
- 
+	let arrFoundIn = findTypeUsedAgainst();
+	
+	/** 
+	* Using the index of the inner array where the type was found
+	* we can determine how much damage will be done against that type.
+	* first array:  no damage
+	* second array: half damage
+	* third array:  double damage
+	* not found:    normal damage
+	**/
+	if (arrFoundIn === 0) {
+		return 0;
+	} else if (arrFoundIn == 1) {
+		return 0.5;
+	} else if (arrFoundIn == 2) {
+		return 2;
+	} else return 1;
+} 
